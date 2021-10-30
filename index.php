e153e7f3ed36afc7c14f8968db2a20bae6949b0d
Glaubersouza1984 <glauberwzs@hotmail.com>
2021-10-29 23:47:07 -0300

    criamos pageadmin e tpl admin

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php 
 
-require_once("vendor/autoload.php");
+require_once("vendor/autoload.php");  // do composer sempre vai existir onde está as pastas do projeto.
 
-use \Slim\Slim;
+use \Slim\Slim; //namespace dentro do vendor tenho dezenas de classes qual eu quero.
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
-$app = new Slim();
+$app = new Slim(); // por conta das rotas criando uma nova aplicação do slim 
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function() { //é uma rota o \ onde ela está
 
 	$page = new Page();
 
@@ -17,6 +18,14 @@ $app->get('/', function() {
 	
 });
 
-$app->run();
+$app->get('/admin/', function() {
 
- ?>
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+	
+});
+
+$app->run(); // pronto tudo carregado, vamos rodar.
+
+?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,6 +1,6 @@
 <?php
 
-  namespace Hcode;
+  namespace Hcode; // onde está classe está
 
   use Rain\Tpl; // o Rain tem o namespace dele mesmo quando chamar o new tpl é do namespace Rain
 
@@ -13,23 +13,23 @@
 
     ];
 
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/"){
 
-      $this->options = array_merge($this->defaults, $opts);
+      $this->options = array_merge($this->defaults, $opts); //vai mesclar os dois arrays e guardar dentor de options 
 
       $config = array(
-        "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+        "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,//variável de ambient $_SERVER vai buscar a pasta no dir raiz
         "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
         "debug"         => false
       );
     
       Tpl::configure( $config );
 
-      $this->tpl = new Tpl;
+      $this->tpl = new Tpl; //para ter acesso configurar como atributo da própria classe.
 
       $this->setData($this->options["data"]);
 
-      $this->tpl->draw("header");
+      $this->tpl->draw("header"); // draw vai desenhar o template na tela espera o nome do arquivo que vai chamar.
 
     }
 
