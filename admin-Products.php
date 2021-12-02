39113c2a83232341453e5666d0015bc1a7b644a2
Glaubersouza1984 <glauberwzs@hotmail.com>
2021-12-01 21:59:40 -0300

    CRUD Produtos

--- admin-Products.php
+++ admin-Products.php
@@ -18,4 +18,77 @@ $app->get("/admin/products", function(){
 
 });
 
+$app->get("/admin/products/create", function(){
+
+  User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-create"); 
+
+});
+
+$app->post("/admin/products/create", function(){// Rota para chamar método que vai criar produto
+
+  User::verifyLogin();
+
+	$product = new Product();  // Nova instância da classe Product
+
+  $product->setData($_POST); // product vamos setar a variável Post vai pegar os mesmos names da variável deste array global post e vai colocar no nosso objeto.
+
+  $product->save(); // salvar no BD.
+
+  header('Location: /admin/products'); //depois de salvo redirecionar para tela produtos.
+	exit;
+
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct){ // HTML para trazer na tela.
+
+	User::verifyLogin();
+
+	$product = new Product(); // criamos a instância desta classe.
+
+	$product->get((int)$idproduct); //Fazer um cast para converter para numérico vem em html em texto.
+	
+	$page = new PageAdmin(); // Tela para carregar as categorias.
+
+	$page->setTpl("products-update", [
+		'product'=>$product->getValues() //Converter em um array.
+	]);
+
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct){ // HTML para trazer na tela.
+
+	User::verifyLogin();
+
+	$product = new Product(); // criamos a instância desta classe.
+
+	$product->get((int)$idproduct); //Fazer um cast para converter para numérico vem em html em texto.
+	
+  $product->setData($_POST);
+
+  $product->save(); 
+  
+  $product->setPhoto($_FILES["file"]);
+
+  header('Location: /admin/products');
+  exit;
+
+});
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct){
+
+	$product = new Product; //Criando um objeto da classe
+
+	$product->get((int)$idproduct); // Carregar este objeto para ter certeza que ele existe lá no banco de dados.
+
+	$product->delete(); // método para deletar.
+
+	header('Location: /admin/products'); //depois de salvo redirecionar para tela categorias.
+	exit;
+
+});
+
 ?>
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -22,15 +22,14 @@ class Product extends Model { // Classe model sabe fazer os geters e seters
 
     $sql = new Sql();
 
-    $results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)",
-     array(
+    $results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
       ":idproduct"=>$this->getidproduct(), 
       ":desproduct"=>$this->getdesproduct(),
       ":vlprice"=>$this->getvlprice(),
       ":vlwidth"=>$this->getvlwidth(),
       ":vlheight"=>$this->getvlheight(),
       ":vllength"=>$this->getvllength(),
-      ":vlweigth"=>$this->getvlweigth(),
+      ":vlweight"=>$this->getvlweight(),
       "desurl"=>$this->getdesurl()
     ));
 
@@ -59,6 +58,80 @@ class Product extends Model { // Classe model sabe fazer os geters e seters
     $sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct",[
       'idproduct'=>$this->getidproduct() // pegar do próprio objeto palavra reservada this.
     ]);
+
+  }
+
+
+  public function checkPhoto()
+  {
+
+    if (file_exists( 
+      $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . //aqui é caminho de pasta do SO.
+      "res" . DIRECTORY_SEPARATOR .
+      "site" . DIRECTORY_SEPARATOR .
+      "img" . DIRECTORY_SEPARATOR .
+      "products" . DIRECTORY_SEPARATOR .
+      $this->getidproduct() . ".jpg"
+      )) {
+
+        $url = "/res/site/img/products/" . $this->getidproduct() . ".jpg"; // aqui é url.
+
+      } else {
+
+        $url = "/res/site/img/product.jpg";
+
+      }
+
+      return $this->setdesphoto($url);
+
+  }
+
+  public function getValues()
+  {
+
+    $this->checkPhoto();
+
+    $values = parent::getValues();
+
+    return $values;
+
+  }
+
+  public function setPhoto($file)
+  {
+
+    $extension = explode('.', $file['name']); // encontrou o ponto fez um array a partir dele. 
+    $extension = end($extension);
+    
+    switch ($extension) {
+
+      case "jpg":
+      case "jpeg":
+        $image = imagecreatefromjpeg($file["tmp_name"]);
+      break;
+
+      case "gif":
+        $image = imagecreatefromgif($file["tmp_name"]);  
+      break;
+
+      case "png":
+        $image = imagecreatefrompng($file["tmp_name"]);
+      break;
+
+    }
+
+    $dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . //aqui é caminho de pasta do SO.
+    "res" . DIRECTORY_SEPARATOR .
+    "site" . DIRECTORY_SEPARATOR .
+    "img" . DIRECTORY_SEPARATOR .
+    "products" . DIRECTORY_SEPARATOR .
+    $this->getidproduct() . ".jpg";
+
+    imagejpeg($image, $dist);
+
+    imagedestroy($image);
+
+    $this->checkPhoto();
 
   }
     
